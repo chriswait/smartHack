377667cd01cf3952d2418ab198ed387a0041325b
Barney Jackson <barney.jackson@live.com>
2013-02-20 00:07:01 +0000

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Ilwhack::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pydwgns#index'
+  root :to => 'doodles#index'
 
   # See how all your routes lay out with "rake routes"
 
